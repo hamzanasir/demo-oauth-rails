e228bf7d39c9d5396206c16de0b3a7995dd2085e
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-04 17:28:33 -0500

    Helper methods for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+  def current_user
+    @current_user ||= User.find_by(uid: session[:user_uid])
+  end
+
+  def current_user=(user)
+    session[:user_uid] = user.uid
+    session[:user_nickname] = user.nickname
+  end
 end
